cad3a30dc290a245becdea19995ceae54ec2f609
Ahmed Magdy <ahmedmagdymorad27@gmail.com>
2026-02-28 19:39:42 +0200

    solve bug in JwtAuthenticationFilter

--- backend/src/main/java/com/movieapp/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/movieapp/security/JwtAuthenticationFilter.java
@@ -52,8 +52,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 );
 
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request, response);
     }
 }
-
